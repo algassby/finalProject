b68d9d06e28ae399ed3d798602c2de70eedbb7fc
algassby <algassby@hotmail.com>
2022-04-15 17:05:09 +0200

    Create executeBack.sh

---
+++ executeBack.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd Back
+
+docker-compose exec -T app php bin/console doctrine:fixtures:load
+docker-compose exec -T app php bin/console doctrine:database:drop --force --env=test
+docker-compose exec -T app php bin/console doctrine:database:create --env=test
+docker-compose exec -T app php bin/console doctrine:migrations:migrate -n --env=test
+docker-compose exec -T app ./vendor/bin/phpunit
+docker exec -e XDEBUG_MODE=coverage php-app vendor/bin/phpunit --coverage-text
