5c09f5a50eb46431a13f7d617803bdc5b27350a2
algassby <algassby@hotmail.com>
2022-04-15 17:13:21 +0200

    Update executeBack.sh

--- executeBack.sh
+++ executeBack.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd Back
 
+docker-compose exec -T app php bin/console doctrine:migration:migrate
 docker-compose exec -T app php bin/console doctrine:fixtures:load
 docker-compose exec -T app php bin/console doctrine:database:drop --force --env=test
 docker-compose exec -T app php bin/console doctrine:database:create --env=test
