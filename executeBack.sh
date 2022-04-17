54b6ee8da5a38a1e318706f3ca68111e58affa44
styve <lotchouang33@yahoo.fr>
2022-04-17 21:38:59 +0200

    resolve conflit

--- executeBack.sh
+++ executeBack.sh
@@ -5,6 +5,6 @@ docker-compose exec -T app php bin/console doctrine:migration:migrate
 docker-compose exec -T app php bin/console doctrine:fixtures:load
 docker-compose exec -T app php bin/console doctrine:database:drop --force --env=test
 docker-compose exec -T app php bin/console doctrine:database:create --env=test
-docker-compose exec -T app php bin/console doctrine:migrations:migrate -n --env=test
+docker-compose exec -T app php bin/console doctrine:migrations:migrate --env=test
 docker-compose exec -T app ./vendor/bin/phpunit
 docker exec -e XDEBUG_MODE=coverage php-app vendor/bin/phpunit --coverage-text
